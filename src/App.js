8d01e37da64a8ac6b7827ccceefef9cb721bb338
Drummberg <drummberg@gmail.com>
2022-05-08 22:19:52 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import styled from 'styled-components';
 
+import { ContactForm } from './components/ContactForm/ContactForm';
 
+export class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  render() {
+    return (
+      <>
+        <Section>
+          <Title>Phonebook</Title>
+          <ContactForm />
+        </Section>
+      </>
+    );
+  }
+}
+
+const Title = styled.h1`
+  margin-bottom: 10px;
+`;
+
+const Section = styled.section`
+  padding: 20px;
+`;
 export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+export class ContactForm extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Label>
+          Name
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Enter name"
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+
+        <Label>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            value={number}
+            country={'ua'}
+            regions={'europe'}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+
+        <Button type="submit">Add contact</Button>
+      </Form>
+    );
+  }
+}
+
+const Form = styled.form`
+  width: 320px;
+  padding: 10px;
+  border: 1px solid black;
+`;
+
+const Input = styled.input`
+  position: relative;
+  font-size: 14px;
+  background: #ffffff;
+  border-radius: 5px;
+  line-height: 25px;
+  height: 35px;
+  width: 300px;
+  padding: 0 10px;
+  outline: none;
+  border: 1px solid #cacaca;
+  margin-bottom: 10px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  border-radius: 5px;
+`;
